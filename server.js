ea9f425e5c8ca50397f1f4b59e3b82ba1d3fb9de
Joshua Sung <yuniksung@gmail.com>
2021-01-16 21:53:33 -0600

    first commit, have created directories and installed npm packages

---
+++ server.js
@@ -0,0 +1,11 @@
+const sequelize = require('./config/connection');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+});
