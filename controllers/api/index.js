79e52bb754ad6f3013e844dfd42912753b493c91
Joshua Sung <yuniksung@gmail.com>
2021-02-02 22:05:49 -0600

    posts -> /post in controller/api/index.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -5,7 +5,7 @@ const postRoutes = require('./post-routes');
 const commentRoutes = require('./comment-routes');
 
 router.use('/user', userRoutes);
-router.use('/posts', postRoutes);
+router.use('/post', postRoutes);
 router.use('/comments', commentRoutes);
 
 module.exports = router;
