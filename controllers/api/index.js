ddbcaa6798ac7f494ffeed5bb6f4ea43e2480439
Joshua Sung <yuniksung@gmail.com>
2021-02-02 22:04:22 -0600

    users to user changed in index.js(controllers/api)

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -4,7 +4,7 @@ const userRoutes = require('./user-routes.js');
 const postRoutes = require('./post-routes');
 const commentRoutes = require('./comment-routes');
 
-router.use('/users', userRoutes);
+router.use('/user', userRoutes);
 router.use('/posts', postRoutes);
 router.use('/comments', commentRoutes);
 
